b27159a543ba06edc16a5b808314e57b93e929d8
manansaini <manan.20bcon217@jecrcu.edu.in>
2024-09-01 23:04:19 +0530

    added crud operation using sql jet

---
+++ internal/models/user.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"context"
+)
+
+type User struct {
+	ID       []byte `json:"id"`
+	UserName string `json:"user_name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+type ReadUserRequest struct {
+	By    string
+	Value string
+}
+
+type UpdateUserParameters struct {
+	UserName string `json:"user_name" validate:"required,max=150"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserParameters struct {
+	UserName string `json:"user_name" validate:"required,max=150"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserManager interface {
+	Read(ctx context.Context, req ReadUserRequest) (*User, error)
+	Create(ctx context.Context, tenantID string, params CreateUserParameters) (*User, error)
+	Delete(ctx context.Context, req ReadUserRequest) (*User, error)
+}
