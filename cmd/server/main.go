c5fa8b3a201b3b28b050447d9000bd0fec0f3953
manansaini <manan.20bcon217@jecrcu.edu.in>
2024-09-02 22:38:37 +0530

    added cancelation signal with cofig file logc in runconfig, changed the structure of main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,35 +1,73 @@
 package main
 
 import (
-	"log"
+	"context"
+	"database/sql"
+	"errors"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/Employes-Side/employee-side/internal/config"
-	"github.com/Employes-Side/employee-side/internal/db"
+	_ "github.com/go-sql-driver/mysql"
+
+	employeside "github.com/Employes-Side/employee-side"
+	"github.com/Employes-Side/employee-side/internal/endpoints"
 	"github.com/Employes-Side/employee-side/internal/handlers"
-	"github.com/joho/godotenv"
+	"github.com/Employes-Side/employee-side/internal/repositories"
+	"github.com/gorilla/mux"
+	"k8s.io/klog"
 )
 
 func main() {
 
-	err := godotenv.Load()
+	cfg := employeside.LoadConfiguration()
+	dbConn, err := sql.Open("mysql", cfg.DB.CreateDSN())
 	if err != nil {
-		log.Println("No .env file found, proceeding with environment variables")
-	}
-
-	cfg := config.LoadConfig()
-
-	dbConn, err := db.Connect(cfg)
-	if err != nil {
-		log.Fatalf("Failed to connect with database: %v", err)
+		panic(err)
 	}
 
 	defer dbConn.Close()
 
-	http.HandleFunc("/users", handlers.UserHandler(dbConn))
-
-	log.Println("Starting HTTP server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	var userManager repositories.UserRepository
+	{
+		userManager = *repositories.NewManager(dbConn)
 	}
+
+	UserEndpoint := endpoints.NewUserEndpoint(userManager)
+
+	router := mux.NewRouter()
+	{
+		appHandler := handlers.NewHandler(UserEndpoint)
+
+		router.PathPrefix("/").Handler(appHandler)
+
+	}
+
+	httpServer := &http.Server{Handler: router}
+	{
+		lis, err := net.Listen("tcp", cfg.Bind.HTTP)
+		if err != nil {
+			panic(err)
+		}
+
+		klog.Infof("starting http server on %q", lis.Addr())
+		go func() {
+			if err := httpServer.Serve(lis); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}()
+	}
+
+	defer func() {
+		klog.Info("closing http server...")
+		klog.Infof("http server close error: %v",
+			httpServer.Shutdown(context.Background()))
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	klog.Infof("received shutdown signal %q", <-sig)
+	klog.Infof("bye")
 }
